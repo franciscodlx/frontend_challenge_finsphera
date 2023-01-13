5b6d5c4ae62de4fde54a6d32adbcca45e875f6e3
franciscodlx <franciscodlx@outlook.es>
2023-01-12 23:34:26 -0600

    [fco][react] Add class to hover in card

--- src/components/characters/character.jsx
+++ src/components/characters/character.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Col, Card } from 'react-bootstrap';
 
 const Character = ({
@@ -6,9 +7,10 @@ const Character = ({
   handleShow,
   name,
 }) => {  
+  const [show, setShow] = useState(false);
   return (
     <Col xs={ 6 } md={ 4 } lg={ 2 } onClick={ () => handleShow(count) } className='my-2'>
-      <Card className='h-100'>
+      <Card className={ `h-100 card-body ${show ? 'card-hover' : ''}` } onMouseEnter={() => setShow(!show)} onMouseLeave={() => setShow(!show)}>
         <Card.Img className='card-img' variant="top" src={ image } />
         <Card.Body className='d-flex justify-content-center align-items-center'>
           <Card.Title className='text-center m-0'>{ name }</Card.Title>
